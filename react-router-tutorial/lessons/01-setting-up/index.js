b7866aed3757130671d464ffc969a2b62b25d8b0
Viswanathan <saravanan.resume@gmail.com>
2017-01-08 20:45:34 +0800

    router

--- react-router-tutorial/lessons/01-setting-up/index.js
+++ react-router-tutorial/lessons/01-setting-up/index.js
@@ -1,27 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 
-import Home from './modules/Home';
-import App from './modules/App';
-import About from './modules/About';
-import Repos from './modules/Repos';
-import Repo from './modules/Repo'
+import routes from './modules/routes';
 
 
 
 render(
-
-(
-	<Router history={browserHistory}>
-	  <Route path="/" component={App} > 
-			<IndexRoute component={Home}/>
-			<Route path="/repos" component={Repos} >
-				<Route path="/repos/:userName/:repoName" component={Repo} />
-			</Route>
-			<Route path="/about" component={About} />
-		</Route>
-	</Router>
-), 
- document.getElementById('app'));
+	<Router  routes={routes} history={browserHistory} />,
+	document.getElementById('app')
+);
 
