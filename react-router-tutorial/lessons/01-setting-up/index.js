f0f893ca7433d808d54fd226267533799d1173f9
Viswanathan <saravanan.resume@gmail.com>
2017-01-08 15:52:55 +0800

    Added Router

--- react-router-tutorial/lessons/01-setting-up/index.js
+++ react-router-tutorial/lessons/01-setting-up/index.js
@@ -1,4 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+
+import { Router, Route, hashHistory } from 'react-router';
+
+render(
+
+(
+	<Router hisory={hashHistory}>
+		<Route path="/" component={App} />
+	</Router>
+), 
+ document.getElementById('app'))
--- react-router-tutorial/lessons/01-setting-up/webpack.config.js
+++ react-router-tutorial/lessons/01-setting-up/webpack.config.js
@@ -8,7 +8,15 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+      { test: /\.js$/, 
+        exclude: /node_modules/, 
+        loader: "babel-loader",
+        query: {
+            presets: ["react", "es2015", "stage-2"],
+            plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],                    
+        }        
+        /* loader: 'babel-loader?presets[]=es2015&presets[]=react' */
+      }
     ]
   }
 }
