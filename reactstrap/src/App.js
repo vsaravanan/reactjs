281a99c402c05064767f60d47a49ca6784d58720
V Saravanan <saravanan.resume@gmail.com>
2018-05-25 20:28:13 +0800

    reactstrap

---
+++ reactstrap/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const element = (
+  <div>
+    testing
+  </div>
+)
+
+export default class App extends React.Component {
+  render() {
+    return ( element) ;
+  }
+}
