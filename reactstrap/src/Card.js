4ed0694d93916e626159841e5ca54fcb4bfb0cf8
V Saravanan <saravanan.resume@gmail.com>
2018-05-27 16:45:16 +0800

    color inverse

--- reactstrap/src/Card.js
+++ reactstrap/src/Card.js
@@ -3,29 +3,40 @@ import { Card, CardImg, CardText, CardBody, CardLink,
   CardTitle, CardSubtitle, Button, Row, Col, CardHeader, CardFooter  } from 'reactstrap';
 
 const Example = (props) => {
-  return (
-    <div>
-      <Card>
-        <CardHeader>Header</CardHeader>
-        <CardBody>
+    return (
+      <div>
+        <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
           <CardTitle>Special Title Treatment</CardTitle>
           <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-          <Button>Go somewhere</Button>
-        </CardBody>
-        <CardFooter>Footer</CardFooter>
-      </Card>
-
-      <Card>
-        <CardHeader tag="h3">Featured</CardHeader>
-        <CardBody>
+          <Button>Button</Button>
+        </Card>
+        <Card body inverse color="primary">
           <CardTitle>Special Title Treatment</CardTitle>
           <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-          <Button>Go somewhere</Button>
-        </CardBody>
-        <CardFooter className="text-muted">Footer</CardFooter>
-      </Card>
-    </div>
-  );
-};
-
-export default Example;
+          <Button color="secondary">Button</Button>
+        </Card>
+        <Card body inverse color="success">
+          <CardTitle>Special Title Treatment</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Button color="secondary">Button</Button>
+        </Card>
+        <Card body inverse color="info">
+          <CardTitle>Special Title Treatment</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Button color="secondary">Button</Button>
+        </Card>
+        <Card body inverse color="warning">
+          <CardTitle>Special Title Treatment</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Button color="secondary">Button</Button>
+        </Card>
+        <Card body inverse color="danger">
+          <CardTitle>Special Title Treatment</CardTitle>
+          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+          <Button color="secondary">Button</Button>
+        </Card>
+      </div>
+    );
+  };
+  
+  export default Example;
