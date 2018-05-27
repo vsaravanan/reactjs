7711b169d3aa41e1792c713fbb617f3776765a74
V Saravanan <saravanan.resume@gmail.com>
2018-05-27 19:06:15 +0800

    Modal form

--- reactstrap/src/App.js
+++ reactstrap/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import Example from 'Card';
+// import React from 'react';
+// import Example from 'Card';
+// import { Button } from 'reactstrap';
 
 
 // const element = (
@@ -8,8 +9,50 @@ import Example from 'Card';
 //   </div>
 // )
 
+// export default class App extends React.Component {
+//   render() {
+//     return ( <Example />) ;
+//   }
+// }
+
+import React from 'react';
+//import 'bootstrap/dist/css/bootstrap.css';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
 export default class App extends React.Component {
-  render() {
-    return ( <Example />) ;
-  }
+
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            modal: false
+        };
+
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle() {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Button color="danger" onClick={this.toggle}> Open Modal </Button>
+                
+                <Modal isOpen={this.state.modal} toggle={this.toggle}>
+                    <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+                    <ModalBody>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+                        <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+        );
+    }
 }
--- reactstrap/src/Card.js
+++ reactstrap/src/Card.js
@@ -1,52 +1,82 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardLink,
-  CardTitle, CardSubtitle, Button, Row, Col, CardHeader, CardFooter, CardColumns  } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import {PropTypes} from 'prop-types';
 
-const Example = (props) => {
-    return (
-      <CardColumns>
-        <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
-          <CardTitle>Special Title Treatment</CardTitle>
-          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-          <Button>Button</Button>
-        </Card>
-        <Card body inverse color="primary">
-          <CardTitle>Special Title Treatment</CardTitle>
-          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-          <Button color="secondary">Button</Button>
-        </Card>
-        <Card body color="primary">
-            <CardTitle>Special Title Treatment</CardTitle>
-            <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-            <Button color="secondary">Button</Button>
-        </Card>
-        <Card body outline color="primary">
-            <CardTitle>Special Title Treatment</CardTitle>
-            <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-            <Button color="secondary">Button</Button>
-        </Card>
-        <Card body inverse color="success">
-          <CardTitle>Special Title Treatment</CardTitle>
-          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-          <Button color="secondary">Button</Button>
-        </Card>
-        <Card body inverse color="info">
-          <CardTitle>Special Title Treatment</CardTitle>
-          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-          <Button color="secondary">Button</Button>
-        </Card>
-        <Card body inverse color="warning">
-          <CardTitle>Special Title Treatment</CardTitle>
-          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-          <Button color="secondary">Button</Button>
-        </Card>
-        <Card body inverse color="danger">
-          <CardTitle>Special Title Treatment</CardTitle>
-          <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-          <Button color="secondary">Button</Button>
-        </Card>
-      </CardColumns>
-    );
-  };
-  
-  export default Example;
+
+export default class Example extends React.Component {
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col>.col</Col>
+                </Row>
+                <Row>
+                    <Col>.col</Col>
+                    <Col>.col</Col>
+                    <Col>.col</Col>
+                    <Col>.col</Col>
+                </Row>
+                <Row>
+                    <Col xs="3">.col-3</Col>
+                    <Col xs="auto">.col-auto - variable width content</Col>
+                    <Col xs="3">.col-3</Col>
+                </Row>
+                <Row>
+                    <Col xs="6">.col-6</Col>
+                    <Col xs="6">.col-6</Col>
+                </Row>
+                <Row>
+                    <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
+                    <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
+                    <Col sm="4">.col .col-sm-4</Col>
+                </Row>
+                <Row>
+                    <Col sm={{ size: 6, push: 2, pull: 2, offset: 1 }}>.col .col-sm-6 .col-sm-push-2 .col-sm-pull-2 .col-sm-offset-2</Col>
+                </Row>
+                <Row>
+                    <Col sm="12" md={{ size: 8, offset: 2 }}>.col .col-sm-12 .col-md-6 .col-md-offset-3</Col>
+                </Row>
+                <Row>
+                    <Col sm={{ size: 'auto', offset: 1 }}>.col .col-sm .col-sm-offset-1</Col>
+                    <Col sm={{ size: 'auto', offset: 1 }}>.col .col-sm .col-sm-offset-1</Col>
+                </Row>
+            </Container>
+        );
+    }
+}
+
+Container.propTypes = {
+  fluid:  PropTypes.bool
+  // applies .container-fluid class
+}
+
+Row.propTypes = {
+  noGutters: PropTypes.bool
+}
+
+const stringOrNumberProp = PropTypes.oneOfType([PropTypes.number, PropTypes.string]);
+const columnProps = PropTypes.oneOfType([
+  PropTypes.string,
+  PropTypes.number,
+  PropTypes.bool,
+  PropTypes.shape({
+    size: PropTypes.oneOfType([PropTypes.bool, PropTypes.number, PropTypes.string]),
+    // example size values:
+    // 12 || "12" => col-12 or col-`width`-12
+    // auto => col-auto or col-`width`-auto
+    // true => col or col-`width`
+    order: stringOrNumberProp,
+    offset: stringOrNumberProp
+  })
+]);
+
+Col.propTypes = {
+  xs: columnProps,
+  sm: columnProps,
+  md: columnProps,
+  lg: columnProps,
+  xl: columnProps,
+  // override the predefined width (the ones above) with your own custom widths.
+  // see https://github.com/reactstrap/reactstrap/issues/297#issuecomment-273556116
+  widths: PropTypes.array,
+}
