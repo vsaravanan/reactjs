ba4f924c4a9e890d4ae8d43582e05eca268c6ce9
V Saravanan <saravanan.resume@gmail.com>
2018-06-12 08:17:50 +0800

    kendo-react-grid

--- react-kendo/src/index.js
+++ react-kendo/src/index.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux' ;
-import { Provider, connect } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { Grid, GridColumn as Column, GridCell } from '@progress/kendo-react-grid';
 
+import products from './app/products.json';
 
+class CustomCell extends GridCell {
+    render() {
+        return (
+            <td>
+                <input disabled type="checkbox" checked={this.props.dataItem[this.props.field]} />
+            </td>
+        );
+    }
+}
 
-const TodoApp = () => 
- (
-      <div>
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            gridData: products
+        };
+    }
+    render() {
+        return (
+            <div>
+                <Grid
+                    style={{ height: '400px' }}
+                    data={this.state.gridData}
+                >
+                    <Column field="ProductID" title="ID" width="40px" />
+                    <Column field="ProductName" title="Name" width="250px" />
+                    <Column field="Category.CategoryName" title="CategoryName" />
+                    <Column field="UnitPrice" title="Price" width="80px" />
+                    <Column field="UnitsInStock" title="In stock" width="80px" />
+                    <Column field="Discontinued" title="Discontinued" width="120px" cell={CustomCell} />
+                </Grid>
+            </div>
+        );
+    }
+}
 
-      </div>
-    );
-
-
-  ReactDOM.render(
-
-    <TodoApp   />,
-    document.getElementById('root')
-  );
-
-
-
-console.log('All tests are passed');
-
+ReactDOM.render(
+    <App />,
+    document.querySelector('my-app')
+);
